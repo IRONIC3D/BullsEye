8eacf4df4a63253d8a66c55758545f6a5ea5ee63
Iyad Horani <i3d@ironic3d.com>
2014-12-14 22:54:09 +1100

    Connect all other labels and add score logic

--- BullsEye/BullsEyeViewController.m
+++ BullsEye/BullsEyeViewController.m
@@ -16,6 +16,8 @@
 {
     int _currentValue;
     int _targetValue;
+    int _score;
+    int _round;
 }
 
 
@@ -33,10 +35,28 @@
 }
 
 - (IBAction)showAlert {
-    NSString *message = [NSString stringWithFormat:@"The value of the slider is: %d\nThe target value is: %d", _currentValue, _targetValue];
+    int difference = abs(_currentValue - _targetValue);
+    int points = 100 - difference;
+
+    NSString *title;
+    if (difference == 0) {
+        title = @"Perfect";
+        points += 100;
+    } else if (difference < 5) {
+        title = @"You almost had it!";
+        points += 50;
+    } else if (difference < 10) {
+        title = @"Pretty good!";
+    } else {
+        title = @"Not even close...";
+    }
+
+    _score += points;
+
+    NSString *message = [NSString stringWithFormat:@"You scored %d points", points];
 
     UIAlertView *alertView = [[UIAlertView alloc]
-            initWithTitle:@"Hello, World"
+            initWithTitle:title
                   message:message
                  delegate:nil
         cancelButtonTitle:@"Awesome"
@@ -53,6 +73,7 @@
 }
 
 - (void)startNewRound {
+    _round += 1;
     _targetValue = 1 + arc4random_uniform(100);
     _currentValue = 50;
     self.slider.value = _currentValue;
@@ -60,6 +81,8 @@
 
 - (void)updateLabels {
     self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
 }
 
 
